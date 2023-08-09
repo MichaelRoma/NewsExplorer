d3e81e2981eb0ad0f3d4a9bab8fd94d043e04ecf
Miha <miromanovsky@gmail.com>
2023-08-09 22:02:37 +0800

    Initial Commit

---
+++ NewsExplorer/NewsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerApp.swift
+//  NewsExplorer
+//
+//  Created by Mikhayl Romanovsky on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
